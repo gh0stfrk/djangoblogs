4e32e15452b8740f6bfdc1002f0971e7745365a6
gh0stfrk <salmansyyd12@gmail.com>
2024-04-24 01:50:53 +0530

    feat: added likes and comment models

--- users/views.py
+++ users/views.py
@@ -1,12 +1,23 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest
+from django.http import HttpRequest, JsonResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 
 from .forms import UserRegisterForm, UploadProfilePhotoForm, UpdateUserForm
 from .models import Profile
 from django.shortcuts import get_object_or_404
+
+
+def user_profile(request: HttpRequest, username: str):
+    user = get_object_or_404(User, username=username)
+    profile = Profile.objects.get(user=user)
+    return JsonResponse(
+        {
+            "user": {"username": user.username, "email": user.email},
+            "profile": {"image": str(profile.image.url)},
+        }
+    )
 
 
 def register(request):
@@ -21,6 +32,7 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
+
 
 def update_user(request):
     user_obj = get_object_or_404(User, id=request.user.id)
@@ -47,5 +59,7 @@ def profile(request: HttpRequest):
             handle_profile_photo(request.user, profile_photo)
     form = UploadProfilePhotoForm()
     update_user = UpdateUserForm(instance=request.user)
-    
-    return render(request, "users/profile.html", {"form": form, "userform": update_user})
+
+    return render(
+        request, "users/profile.html", {"form": form, "userform": update_user}
+    )
